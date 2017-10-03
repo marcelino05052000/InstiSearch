0a1720880e695111bd6aa4b13dbd681c06810903
marcelino05052000 <marcelino05052000@gmail.com>
2017-10-03 00:16:05 -0300

    Mudança no input do submit para primeng

--- src/app/template-form/template-form.module.ts
+++ src/app/template-form/template-form.module.ts
@@ -1,3 +1,4 @@
+import { ButtonModule } from 'primeng/primeng';
 import { TemplateFormComponent } from './template-form.component';
 import { FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
@@ -6,7 +7,8 @@ import { CommonModule } from '@angular/common';
 @NgModule({
   imports: [
     CommonModule,
-    FormsModule
+    FormsModule,
+    ButtonModule
   ],
   declarations: [
     TemplateFormComponent
