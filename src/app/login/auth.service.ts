b2f91bbada08e78200f78bb26eeefb5ac5fcd6ef
marcelino05052000 <marcelino05052000@gmail.com>
2017-10-09 23:08:35 -0300

    ajuste no login

--- src/app/login/auth.service.ts
+++ src/app/login/auth.service.ts
@@ -1,8 +1,22 @@
+import { Router } from '@angular/router'
+import { Usuario } from './usuario';
 import { Injectable } from '@angular/core';
 
 @Injectable()
 export class AuthService {
 
-  constructor() { }
+  private usuarioAutenticado: boolean = false;
 
+  constructor(private router: Router) { }
+
+  fazerLogin(usuario: Usuario) {
+    if(usuario.nome === 'usuario@email.com' &&
+      usuario.senha === '123456') {
+        this.usuarioAutenticado = true;
+
+        this.router.navigate(['/'])
+    } else {
+      this.usuarioAutenticado = false;
+    }
+  }
 }
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,3 +1,4 @@
+import { Usuario } from './usuario';
 import { AuthService } from './auth.service';
 import { Component, OnInit } from '@angular/core';
 
@@ -8,12 +9,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
+  private usuario: Usuario = new Usuario();
+
   constructor(private authService: AuthService) { }
 
   ngOnInit() {
   }
 
   fazerLogin() {
+    //console.log(this.usuario)
+    this.authService.fazerLogin(this.usuario);
   }
 
 }
