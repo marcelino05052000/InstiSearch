a64342e3df361e52ad4f51e8bea80cdcb406e2ee
marcelino05052000 <marcelino05052000@gmail.com>
2017-10-09 22:06:47 -0300

    alteração no package.json e criação do login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,3 +1,4 @@
+import { LoginComponent } from './login/login.component';
 import { MainComponent } from './main/main.component';
 import { TemplateFormComponent } from './template-form/template-form.component';
 import { NgModule } from '@angular/core';
@@ -6,6 +7,7 @@ import { Routes, RouterModule } from '@angular/router';
 const routes: Routes = [
   { path: 'main', component: MainComponent},
   { path: 'templateForm', component: TemplateFormComponent},
+  { path: 'login', component: LoginComponent},
   { path: '', pathMatch: 'full', redirectTo: 'main' }
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,23 @@
+import { AuthService } from './login/auth.service';
 import { TemplateFormModule } from './template-form/template-form.module';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {CalendarModule} from 'primeng/primeng';
+import {PasswordModule} from 'primeng/primeng';
 
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MainComponent } from './main/main.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MainComponent
+    MainComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
@@ -21,9 +25,10 @@ import { MainComponent } from './main/main.component';
     AppRoutingModule,
     FormsModule,
     TemplateFormModule,
-    CalendarModule
+    CalendarModule,
+    PasswordModule
   ],
-  providers: [],
+  providers: [AuthService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
